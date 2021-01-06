4f4b71ef8c33692433f830209d15016f9a0b10ec
Eric Gertonson <eric.gert@gmail.com>
2021-01-06 14:51:46 -0600

    added get_current_values method to utils

--- code/allow_utils.py
+++ code/allow_utils.py
@@ -2,4 +2,20 @@ import allow_conn as con
 
 def get_current_weekend():
     current_date =  con.execute_query("Select week_end_date from weekends where current_record_ind = 'Y';")
-    return current_date
+    return str(current_date[0][0])
+
+
+def get_current_values():
+    query_sql = """
+    SELECT week_end_date, child_name, amount
+    FROM allowance_details
+    WHERE week_end_date = (
+	    select week_end_date 
+	    from weekends
+	    where current_record_ind = 'Y'
+	    )
+    ;
+    """
+    rows = con.execute_query(query_sql)
+    return rows
+
