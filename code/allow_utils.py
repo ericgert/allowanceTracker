a62c8b20986a1d4149ebcae3315668be7c8fa25c
Eric Gertonson <eric.gert@gmail.com>
2021-01-06 21:11:38 -0600

    added the finalize week code and pages

--- code/allow_utils.py
+++ code/allow_utils.py
@@ -5,6 +5,9 @@ def get_current_weekend():
     current_date =  con.execute_query("Select week_end_date from weekends where current_record_ind = 'Y';")
     return str(current_date[0][0])
 
+def get_next_weekend():
+    next_date =  con.execute_query("Select week_end_date + 7 from weekends where current_record_ind = 'Y';")
+    return str(next_date[0][0])
 
 def get_current_values():
     query_sql = """
--- code/app.py
+++ code/app.py
@@ -52,6 +52,47 @@ def modifyValues():
     cur_date = allow_utils.get_current_weekend()
     return render_template('modifyValues.html', WEEK_END_DATE=cur_date)
 
+@app.route('/finalizeWeek', methods=['GET','POST'])
+def finalizeWeek():
+    cur_date = allow_utils.get_current_weekend()    
+    next_date = allow_utils.get_next_weekend()
+    if request.method == 'POST':
+        #format the current date
+        cur_date_str = "'"+cur_date+"'"
+        next_date_str = "'"+next_date+"'"
+        #prepare the update and insert statements and run them to finalize the week and initialize the next
+        update_sql = """
+        update weekends set current_record_ind = 'N' 
+        where week_end_date = {});
+        update weekends set current_record_ind = 'Y'
+        where week_end_date = {};
+        """.format(cur_date_str, next_date_str)
+        #execute the statement and check teh results
+        result = allow_conn.execute_statement(update_sql)
+        if result['code'] == 0:
+            #initialize the new week
+            init_sql = """
+            insert into amount_details (week_end_date, child_name, modified_amount, modify_reason)
+            (select w.week_end_date, da.child_name, da.amount, 'Starting Amount' as modify_reason
+            from weekends w
+            , def_allow da
+            where w.current_record_ind = 'Y')
+            """
+            #execute the statement and check the results
+            init_result = allow_conn.execute_statement(init_sql)
+            if init_result['code'] == 0:
+                message = "The week of {} has been finalized and the week of {} has been initialized".format(cur_date, next_date)
+            else:
+                message = "The week was finalized, however the new week could not be initialized.  Please contact the admin for support."
+        else:
+            message = result['message']
+        return redirect(url_for('message', message=message))
+
+
+
+    return render_template('finalizeWeek.html', WEEK_END_DATE=cur_date)
+
+
 @app.route('/message')
 def message():
     message = request.args['message']
