3ee536819b612aba5fb02eb2f508494dd76d5280
Eric Gertonson <eric.gert@gmail.com>
2021-01-11 10:19:11 -0600

    added show activity with dummy values

--- code/app.py
+++ code/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, url_for, redirect, render_template, session
 import allow_utils
 import allow_conn
+import json
 
 app = Flask(__name__)
 
@@ -93,10 +94,47 @@ def finalizeWeek():
     return render_template('finalizeWeek.html', WEEK_END_DATE=cur_date)
 
 
+@app.route('/priorActivity', methods=['GET','POST'])
+def priorActivity():
+    if request.method == 'POST':
+        # #get values from the form
+        # child = "'"+request.values.get('childSelect')+"'"
+        # amount = request.values.get('modifyAmount')
+        # reason = "'"+request.values.get('modifyReason')+"'"
+        # #grab the current date
+        # cur_date = "'"+allow_utils.get_current_weekend()+"'"
+        # #prepare the insert statement to update the amount
+        # sql = """
+        # insert into amount_details values({}, {}, {}, {})
+        # """.format(cur_date, child, amount, reason)
+        # #execute the statement and check teh results
+        # result = allow_conn.execute_statement(sql)
+        # if result['code'] == 0:
+        #     message = 'You have successfully modified the allowance for {} by ${}'.format(child, amount)
+        # else:
+        #     message = result['message'] 
+        message = '{"date":"2020-01-17","child":"All"}'
+        return redirect(url_for('showActivity', message=message))
+
+
+    cur_date = allow_utils.get_current_weekend()
+    return render_template('modifyValues.html', WEEK_END_DATE=cur_date)
+
 @app.route('/message')
 def message():
     message = request.args['message']
+
     return render_template('message.html', MESSAGE_TEXT=message)
+
+@app.route('/showActivity')
+def showActivity():
+    str_message = request.args['message']
+    msg_dict = json.loads(str_message)
+    cur_date = msg_dict['date']
+    cur_child = msg_dict['child']
+    activity_list = [('date', 'kid', 'amount', 'reason'), ('date1', 'kid1', 'amount1', 'reason1')]
+    activity_len = len(activity_list)
+    return render_template('showActivity.html', cur_date = cur_date, cur_child = cur_child, activity = activity_list, len = activity_len)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
